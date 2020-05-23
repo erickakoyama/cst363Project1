8c88f6e43076547f35cc5ab55787b554d05b5324
Ericka Koyama <ericka.koyama@gmail.com>
2020-05-22 21:31:54 -0700

    initial commit

---
+++ src/test/java/cst363/DrugApplicationTests.java
@@ -0,0 +1,13 @@
+package cst363;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
