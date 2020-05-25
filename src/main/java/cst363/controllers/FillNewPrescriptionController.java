fa6cbefe0dd33e35c39fe788e2c5aa12827e8066
Justin Thomas <justhomas@csumb.edu>
2020-05-24 17:41:23 -0700

    Logically sound code, need sample data to test

--- src/main/java/cst363/controllers/FillNewPrescriptionController.java
+++ src/main/java/cst363/controllers/FillNewPrescriptionController.java
@@ -2,6 +2,7 @@ package cst363.controllers;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,35 +38,73 @@ public class FillNewPrescriptionController
     * 
     * @return confirmation page
     */
-   @PostMapping("/PrescriptionDetails")
+   @GetMapping("/PrescriptionDetails")
    public String getPrescriptionDetails(@RequestParam("ssn") String ssn,
          @RequestParam("prescription_number") String prescNum, Model model)
    {
-      // TODO: get prescription details
-    
+      // format of dssn-pssn-genericName
+      String[] parsePresc = prescNum.split("-");
+
+      PrescriptionDetails details = new PrescriptionDetails();
+      
       try
       {
          Connection con = jdbcTemplate.getDataSource().getConnection();
-         PreparedStatement ps = con.prepareStatement("");
+         
+         /*
+          * select 
+   concat(p.first_name, " ", p.last_name) as patient_name,
+   @pharm = p.prefered_pharmacy,
+   concat(d.first_name, " ", d.last_name) as doctor_name,
+   drugs.generic_name as drug_name,
+   @drugId = drugs.drug_id,
+   c.price as cost
+from 
+   patients p, doctors d, drugs drugs, drug_price c
+where
+   d.doctor_ssn = ? AND
+   p.patient_ssn = ? AND
+   drugs.generic_name = ? AND
+   c.pharmacy_id = @pharm AND
+   c.drug_id = @drugId
+   
+   */
+         // query statement
+         PreparedStatement ps = con.prepareStatement("select \r\n" + 
+               "   concat(p.first_name, \" \", p.last_name) as patient_name,\r\n" + 
+               "   @pharm = p.prefered_pharmacy,\r\n" + 
+               "   concat(d.first_name, \" \", d.last_name) as doctor_name,\r\n" + 
+               "   drugs.generic_name as drug_name,\r\n" + 
+               "   @drugId = drugs.drug_id,\r\n" + 
+               "   c.price as cost\r\n" + 
+               "from \r\n" + 
+               "   patients p, doctors d, drugs drugs, drug_price c\r\n" + 
+               "where\r\n" + 
+               "   d.doctor_ssn = ? AND\r\n" + 
+               "   p.patient_ssn = ? AND\r\n" + 
+               "   drugs.generic_name = ? AND\r\n" + 
+               "   c.pharmacy_id = @pharm AND\r\n" + 
+               "   c.drug_id = @drugId");
+         
+         // set query values based on parsed
+         ps.setString(1, parsePresc[0]);
+         ps.setString(2, parsePresc[1]);         
+         ps.setString(3, parsePresc[2]);
+         
+         // assign results to object
+         ResultSet rs = ps.executeQuery();
+         details.name = rs.getString(1);
+         details.doctor = rs.getString(2);
+         details.drugName = rs.getString(3);
+         details.rxCost = rs.getDouble(4);  
+         
+         model.addAttribute("detailsModel", details.rxCost);
       } catch (SQLException e)
       {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
+         System.out.println("Error:  Prescription#prescription/new SQLException " + e.getMessage());
+         model.addAttribute("msg", e.getMessage());
+         return "error";
       }
-
-      // set query details in model
-      PrescriptionDetails details = new PrescriptionDetails();
-
-      // add model details to view
-      model.addAttribute("cost", details.prescriptionCost);
-      model.addAttribute("fname", details.firstName);
-      model.addAttribute("lname", details.lastName);
-      model.addAttribute("prescName", details.prescriptionName);
-      model.addAttribute("refills", details.availableRefills);
-      model.addAttribute("doctor", details.doctor);
-      
-      // possible to add the entire model to the model attribute
-      
 
       return "PrescriptionDetailsView";
    }
@@ -87,12 +126,10 @@ public class FillNewPrescriptionController
     */
    protected class PrescriptionDetails
    {
-      public String prescriptionName;
-      public String firstName;
-      public String lastName;
+      public String name;
       public String doctor;
-      public double prescriptionCost;
-      public int availableRefills;
+      public String drugName;
+      public double rxCost;
    }
 
 }// end class
