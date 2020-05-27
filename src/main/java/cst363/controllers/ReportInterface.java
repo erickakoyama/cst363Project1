d243481e40d3388c0cf62d514163b539d9269164
abeebe-csumb <abeebe@csumb.edu>
2020-05-26 20:08:54 -0700

    Add new module for fda report.

--- src/main/java/cst363/controllers/ReportInterface.java
+++ src/main/java/cst363/controllers/ReportInterface.java
@@ -3,6 +3,7 @@ package cst363.controllers;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -19,29 +20,73 @@ import cst363.model.Pharmacy;
 public class ReportInterface {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
- 
+	
 	@GetMapping("/reports")
-	public String displayForm(Model model) {
-		try {
-		Connection conn = jdbcTemplate.getDataSource().getConnection();
-
-		String sql = "select\n" + 
-				"	distinct\n" + 
-				"	p.name as name,\n" + 
-				"    p.pharmacy_id as pharmacy_id\n" + 
-				"from prescriptions pr\n" + 
-				"inner join pharmacy p on pr.pharmacy_id = p.pharmacy_id\n" + 
-				"order by 2;";
-		
-		 List<Pharmacy> pharmacyId = jdbcTemplate.query(
-				 sql, new Object[] {} ,
-				 (rs, rowNum) -> new Pharmacy(rs.getString("name"),
-				                               rs.getString("pharmacy_id")));
-		 
-		 model.addAttribute("pharmacyId", pharmacyId);
-				   
-		conn.close();
+	public String loadDefaultPage(Model model){
 		return "reports";
+	}
+ 
+	@GetMapping("/reports/select")
+	public String getReportDisplay(@RequestParam("report_name") String report_name, Model model) {
+		if(report_name.equals("pr_by_pharma")) {
+			try {
+				Connection conn = jdbcTemplate.getDataSource().getConnection();
+	
+				String sql = "select\n" + 
+						"	distinct\n" + 
+						"	p.name as name,\n" + 
+						"    p.pharmacy_id as pharmacy_id\n" + 
+						"from prescriptions pr\n" + 
+						"inner join pharmacy p on pr.pharmacy_id = p.pharmacy_id\n" + 
+						"order by 2;";
+				
+				 List<Pharmacy> pharmacyId = jdbcTemplate.query(
+						 sql, new Object[] {} ,
+						 (rs, rowNum) -> new Pharmacy(rs.getString("name"),
+						                               rs.getString("pharmacy_id")));
+				 
+			 	model.addAttribute("pharmacyId", pharmacyId);
+					   
+				conn.close();
+				return "pr_by_pharma";
+			} catch (SQLException se) {
+				System.out.println("Error");
+				model.addAttribute("msg", se.getMessage());
+				return "error";
+			}
+		
+		} else if (report_name.equals("pr_by_doc")) { 
+			return "pr_by_doc";
+		} else {
+			return "reports";
+		}
+	}
+	@PostMapping("/reports/select/pr_by_doc")
+	public String processPrByDoc(
+			@RequestParam("start_date_input") String start_date,
+			@RequestParam("end_date_input") String end_date,
+			Model model) {
+		try {
+			Connection conn = jdbcTemplate.getDataSource().getConnection();
+	
+			String sql = "SELECT CONCAT(last_name,', ',first_name) as full_name, trade_name, generic_name, SUM(quantity) as quantity\n" + 
+					"FROM doctors d\n" + 
+					"JOIN prescriptions p ON d.doctor_id = p.doctor_id\n" + 
+					"JOIN drugs dgs ON dgs.drug_id = p.drug_id\n" + 
+					"WHERE prescription_date between ? and ?\n" + 
+					"GROUP BY 1, 2, 3;";
+			
+			 List<CONTROLLER> prescriptions = jdbcTemplate.query(
+					 sql, new Object[] {start_date, end_date} ,
+					 (rs, rowNum) -> new {{CONTROLLER}}(rs.getString("full_name"),
+					                               rs.getString("trade_name"),
+					                               rs.getString("generic_name"),
+					                               rs.getInt("quantity"));
+			 
+			 model.addAttribute("prescriptions", prescriptions);
+					   
+			conn.close();
+			return "reportsuccess";
 		} catch (SQLException se) {
 			System.out.println("Error");
 			model.addAttribute("msg", se.getMessage());
@@ -49,15 +94,15 @@ public class ReportInterface {
 		}
 	}
 	
-	@PostMapping("/reports")
-	public String processForm(@RequestParam("start_date_input") String start_date,
+	@PostMapping("/reports/select/pr_by_pharma")
+	public String processPrByPharma( 
+			@RequestParam("start_date_input") String start_date,
 			@RequestParam("end_date_input") String end_date, 
-			@RequestParam("pharmacy_id") String pharmacy_id, 
+			@RequestParam("pharmacy_id") String pharmacy_id,
 			Model model) {
-		
 		try {
 			Connection conn = jdbcTemplate.getDataSource().getConnection();
-
+	
 			String sql = "select\n" + 
 					"	p.name as name,\n" + 
 					"	d.trade_name as trade_name,\n" + 
@@ -72,7 +117,7 @@ public class ReportInterface {
 					"1, 2;";
 			
 			 List<PharmacyPrescriptions> prescriptions = jdbcTemplate.query(
-					 sql, new Object[] {start_date, end_date, pharmacy_id } ,
+					 sql, new Object[] {start_date, end_date, pharmacy_id} ,
 					 (rs, rowNum) -> new PharmacyPrescriptions(rs.getString("name"),
 					                               rs.getString("trade_name"),
 					                               rs.getInt("quantity")));
@@ -80,8 +125,7 @@ public class ReportInterface {
 			 model.addAttribute("prescriptions", prescriptions);
 					   
 			conn.close();
-			return "/reportsuccess";
-			
+			return "reportsuccess";
 		} catch (SQLException se) {
 			System.out.println("Error");
 			model.addAttribute("msg", se.getMessage());
